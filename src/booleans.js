6ba7901e57e60aad022eee916edea990446020af
benji6 <benhall22@hotmail.co.uk>
2015-12-12 14:19:22 +0000

    If is just I**

--- src/booleans.js
+++ src/booleans.js
@@ -1,6 +1,6 @@
 // Check out my [combinators-js](https://github.com/benji6/combinators-js) library for definitions of these functions.
 // A [combinator](https://en.wikipedia.org/wiki/Combinatory_logic) is just a higher order function that uses only function application and earlier defined combinators to define a result from its arguments. Don't worry about the theory surrounding them, you only need to be able to read the definitions to understand this code. The reason I'm importing them is because they are highly useful and reusable when defining the functions in this library.
-import {C, K, I} from 'combinators-js'
+import {C, K, I, I__} from 'combinators-js'
 
 // True takes 2 arguments and returns the first
 export const True = K
@@ -8,7 +8,7 @@ export const True = K
 export const False = K(I)
 
 // If take a predicate and two values, returning the first value if the predicate is True and the second if the predicate is False
-export const If = a => b => c => a(b)(c)
+export const If = I__
 
 export const and = a => b => a(b)(a)
 export const or = a => b => a(a)(b)
