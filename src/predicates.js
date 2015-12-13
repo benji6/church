0deb28e9e0a19124e809d6edd58973a4191a3db3
benji6 <benhall22@hotmail.co.uk>
2015-12-13 12:24:27 +0000

    add lte and eq

--- src/predicates.js
+++ src/predicates.js
@@ -1,4 +1,5 @@
-import {True, False} from './booleans'
+import {and, True, False} from './booleans'
+import {sub} from './numerals'
 
 // `isZero` takes a value and returns Church encoded `True` if it is a Church encoded `zero` and `False` otherwise
 // ```javascript
@@ -6,3 +7,18 @@ import {True, False} from './booleans'
 // isZero(one) // => False
 // ```
 export const isZero = a => a(_ => False)(True)
+
+// `leq` takes two numerals and returns True if the first is less than or equal to the first and False otherwise
+// ```javascript
+// leq(one)(three) // => True
+// leq(three)(three) // => True
+// leq(four)(three) // => False
+// ```
+export const lte = a => b => isZero(sub(a)(b))
+
+// `eq` takes two numerals and returns True if the first is equal to the first and False otherwise
+// ```javascript
+// eq(three)(three) // => True
+// eq(four)(three) // => False
+// ```
+export const eq = a => b => and(lte(a)(b))(lte(b)(a))
--- test/predicates.js
+++ test/predicates.js
@@ -2,10 +2,13 @@ import test from 'tape'
 import{decodeBoolean} from './_tools'
 import {
 	isZero,
+  lte,
+  eq,
   zero,
   one,
   two,
   three,
+  four,
 } from '../src'
 
 test('Predicates - isZero', t => {
@@ -13,5 +16,19 @@ test('Predicates - isZero', t => {
 	t.equal(decodeBoolean(isZero(one)), false)
 	t.equal(decodeBoolean(isZero(two)), false)
 	t.equal(decodeBoolean(isZero(three)), false)
+	t.end()
+})
+
+test('Predicates - lte', t => {
+	t.equal(decodeBoolean(lte(one)(three)), true)
+	t.equal(decodeBoolean(lte(three)(three)), true)
+	t.equal(decodeBoolean(lte(four)(three)), false)
+	t.end()
+})
+
+test('Predicates - eq', t => {
+	t.equal(decodeBoolean(eq(two)(three)), false)
+	t.equal(decodeBoolean(eq(three)(three)), true)
+	t.equal(decodeBoolean(eq(four)(three)), false)
 	t.end()
 })
