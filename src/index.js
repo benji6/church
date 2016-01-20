dbd09bfcbb6113eb43711a9a16c0fb3375577144
benji6 <benhall22@hotmail.co.uk>
2016-01-20 17:38:12 +0000

    docs(index) add reading order tip

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // Welcome to the docs!
 // Use the menu to navigate.
-// All functions exported are curried so you must apply arguments one at a time.
-// Have a look at booleans first.
+// All functions exported are curried so you must apply arguments one at a time
+//
+// Concepts are often built upon and functions defined in terms of previously
+// defined functions so recommended reading order is the order these modules
+// are exported
 export * from './booleans'
 export * from './numerals'
 export * from './predicates'
